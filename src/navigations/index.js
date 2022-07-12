e977b9d6a1efbed2dfe6a5b5e6c12526329a89e8
duypham <xduy.pham@gmail.com>
2022-07-12 15:21:18 +0700

    feat: [init navigation]

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,1 +1,56 @@
-export default main = () => {};
+import {Navigation} from 'react-native-navigation';
+import {COLORS} from 'themes/colors';
+
+export function push({
+  id,
+  screen,
+  title,
+  isTopBarEnable,
+  isBottomTabsEnable,
+  rightButtonIcon,
+  elevation,
+  passProps,
+  textRightTop,
+  disabledRight,
+  colorBtnRight,
+}) {
+  Navigation.push(id, {
+    component: {
+      name: screen,
+      passProps: passProps,
+      options: {
+        bottomTabs: {
+          visible: isBottomTabsEnable,
+          drawBehind: !isBottomTabsEnable,
+        },
+        topBar: {
+          visible: isTopBarEnable,
+          title: {
+            text: title,
+            alignment: 'center',
+            fontSize: 16,
+            fontFamily: '',
+          },
+          rightButtons:
+            rightButtonIcon || textRightTop
+              ? [
+                  {
+                    id: rightButtonIcon || textRightTop,
+                    text: textRightTop,
+                    enabled: disabledRight,
+                    disabledColor: COLORS.neutral2,
+                    icon: '',
+                    color: colorBtnRight,
+                  },
+                ]
+              : [],
+          elevation: elevation,
+          backButton: {
+            showTitle: false,
+            color: COLORS.white,
+          },
+        },
+      },
+    },
+  });
+}
